2624a212fa89d4c6be2bf1f7a55c1cd8176fa76f
lucasluiz19 <lucas844@outlook.com.br>
2022-05-04 17:38:08 -0300

    new_commit

--- script.js
+++ script.js
@@ -2,24 +2,24 @@ function carregar(){
     let msg=window.document.getElementById('test')
     let photo =window.document.getElementById('fotoedit')
     let hours=new Date()
-    let horas=hours.getHours()
+    let horas=17//hours.getHours()
     let minutes=hours.getMinutes()
     msg.innerHTML=`Agora são ${horas} horas`
 
     if(horas >=0 && horas<12){
        msg.innerHTML=`Agora são ${horas}:${minutes} horas, tenha um Bom dia`
-       photo.src='manha.jpg'
+       photo.src='./assets//manha.jpg'
        
 
     }else if(horas >=12 && horas <18) {
         msg.innerHTML=`Agora são ${horas}:${minutes} horas, tenha uma Boa tarde`
-        photo.src='tarde.jpg'
+        photo.src='./assets//tarde.jpg'
         
     
 
     }else{
         msg.innerHTML=`Agora são ${horas}:${minutes} horas, tenha uma Boa noite`
-        photo.src='noite.jpg'
+        photo.src='./assets//noite.jpg'
 
         
     }
